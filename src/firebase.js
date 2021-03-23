a5908369f3f99c0beae9fef8db9470212981d655
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2021-03-23 23:26:38 +0900

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBrNoVzZmLta0p0eeV14lg41JpxXqqagcI",
+    authDomain: "nwitter-52baf.firebaseapp.com",
+    projectId: "nwitter-52baf",
+    storageBucket: "nwitter-52baf.appspot.com",
+    messagingSenderId: "690443110669",
+    appId: "1:690443110669:web:6b079c4d9726b6419b0844"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
