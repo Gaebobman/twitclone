451d821fcdec12595dc9b6207f641b7f0c131396
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2021-03-24 20:43:42 +0900

    Components and Routes / Router Setting

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBrNoVzZmLta0p0eeV14lg41JpxXqqagcI",
-    authDomain: "nwitter-52baf.firebaseapp.com",
-    projectId: "nwitter-52baf",
-    storageBucket: "nwitter-52baf.appspot.com",
-    messagingSenderId: "690443110669",
-    appId: "1:690443110669:web:6b079c4d9726b6419b0844"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
 
   export default firebase.initializeApp(firebaseConfig);
